f013c2fb2c940c6c8be3500a578e0a43554bb88d
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2024-02-09 14:02:22 -0500

    Valorant randomizer uploaded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Component } from 'react';
+import ascent from ".//images/ascent.webp"
+import bind from ".//images/bind.webp"
+import breeze from ".//images/breeze.webp"
+import fracture from ".//images/fracture.webp"
+import haven from ".//images/haven.jpeg"
+import icebox from ".//images/icebox.webp"
+import pearl from ".//images/pearl.webp"
+
 
 function App() {
+
+
+    const maps = ['Haven', 'Ascent', 'Breeze', 'Bind', 'Icebox', 'Fracture', 'Pearl']
+
+    const outputRandomArrayName = (array) => {
+        const lengthOfArray = array.length
+        const randomIndex = Math.floor(Math.random() * lengthOfArray)
+        return array[randomIndex]
+    }
+
+    const mapImgs = [haven, ascent, breeze, bind, icebox, fracture, pearl]
+
+
+
+    const [randomMap, setRandomMap] = useState('');
+
+    useEffect(() => {
+
+    }, [randomMap])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Valorant Map Randomizer</h1>
+        <p>{randomMap ? <p>{randomMap}</p> : <p>Click Button to randomize map</p>}</p>
+
+        <button onClick={() => {
+            setRandomMap(outputRandomArrayName(maps))
+        }}> Randomize Map
+        </button>
+
+        <div>
+        <img src={mapImgs[maps.indexOf(randomMap)]}/>
+        </div>
+
+        
+
+
+
     </div>
+
+
+
   );
 }
 
